81a65357ab5358265fa822c5e363c8a5db3825a6
Lucas <lucas_klasa@hotmail.com>
2025-04-27 14:49:47 -0300

    refactor: union find impl

--- union-find/index.py
+++ union-find/index.py
@@ -4,64 +4,73 @@ class UF:
   def __init__(self, n: int):
     self.n = n
     self.objects = [v for v in range(0, n)]
-    self.components: list[set] = []
 
-  def union(self, p: int, q: int):
+  def union(self, p: int, q: int) -> list[int]:
+    length = len(self.objects)
 
-    if p not in self.objects:
-      return 
-    if q not in self.objects:
+    if p > length:
       return
 
-    p_component = None
-    q_component = None
+    if q > length:
+      return
 
-    for index, component in enumerate(self.components):
-      if p in component:
-        p_component = index
-      if q in component:
-        q_component = index
-      
-      if p_component != None and q_component != None:
-        break
-    
-    if p_component != None and q_component != None:
-      self.components[p_component] = self.components[p_component] | self.components[q_component]
-      self.components.pop(q_component)
-    elif p_component != None and q_component == None:
-      self.components[p_component].add(q)
-    elif p_component == None and q_component != None:
-      self.components[q_component].add(p)
-    else:
-      self.components.append(set((p, q)))
+    p_id = self.objects[p]
+    q_id = self.objects[q]
 
+    for i, id in enumerate(self.objects):
+      if id == q_id:
+        self.objects[i] = p_id
+
+    return self.objects
+
+  def connected(self, p: int, q: int) -> bool:
+    return self.objects[p] == self.objects[q]
   
-  def connected(self, p: int, q: int):
-    for component in self.components:
-      if p in component and q in component:
-        return True
-    
-    return False
+  def find(self, p: int) -> int:
+    return self.objects[p]
+  
+  def count(self) -> int:
+    components = set()
+
+    for id in self.objects:
+      components.add(id)
+
+    return len(components)
+
 
 n = int(input("Initialize the value of N: "))
 uf = UF(n)
 
+print(uf.objects)
+
 while True:
-  print("c to check connection")
-  print("u to perform an union")
-  option = str(input("Press c or u: "))
-  os.system("clear")
+  print(f"Current component: {uf.objects}")
+  print("[connect] to check connection")
+  print("[union] to perform an union")
+  print("[find] to check id of number")
+  print("[count] to check how many ids exists")
   
-  p = int(input("Value of p: "))
-  q = int(input("Value of q: "))
+  option = str(input("[type...]: "))
+  os.system("clear")
 
   match option:
-    case "c":
+    case "connect":
+      p = int(input("Value of p: "))
+      q = int(input("Value of q: "))
+
       is_connected = uf.connected(p, q)
       print(f"{p} and {q} connection is: {is_connected}")
-    case "u":
+    case "union":
+      p = int(input("Value of p: "))
+      q = int(input("Value of q: "))
       uf.union(p, q)
+    case "find":
+      p = int(input("Value of p: "))
+      find = uf.find(p)
+      print(f"[find] result is: {find}")
+    case "count":
+      count = uf.count()
+      print(f"[count] result is: {count}")
 
-  print(f"Current component: {uf.components}")
   print("\n")
 
